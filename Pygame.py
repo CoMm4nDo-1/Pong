ef2c337f4a5081fd8daa0dbee7b74a09a9020fb8
CoMm4nDo-1 <cjanderson2@bellsouth.net>
2026-03-19 22:53:00 -0400

    Updated display screen and added comments

--- Pygame.py
+++ Pygame.py
@@ -1,3 +1,5 @@
+from cmath import rect
+
 import pygame
 import sys
 
@@ -16,13 +18,20 @@ clock = pygame.time.Clock()
 # Defines class of paddle
 class Paddle:
     def __init__(self):
-        self.x = 200
-        self.y = 200
+        # Starting position x and y
+        self.x = 0
+        self.y = 350
+        # Direction of movement in y direction (up and down)
+        self.ydir = 0
+        # Size of the paddle
         self.width = 20
-        self.height = 100
+        self.height = 85
+        # Color of the paddle
         self.color = ("#FFFFFF")
+        # draws the paddle on the screen
         rect = pygame.Rect(self.x, self.y, self.width, self.height)
         pygame.draw.rect(screen, self.color, rect)
+        
 
 paddle = Paddle()
 
@@ -30,10 +39,14 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-            
+            sys.exit()        
     #determines how many fps game will run at
-    clock.tick(10) # 60 fps     
+    clock.tick(10) # 60 fps
+    #updates the screen
+    pygame.display.update()
+    
+
+
 
 
 
